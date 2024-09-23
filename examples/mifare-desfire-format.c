48bd046ae4c4d76ab9402e3259d1b938677308d3
Ingo Albrecht <commit@promovicz.org>
2024-09-23 12:46:44 +0200

    INCOMPLETE Revamp mifare-desfire-format

--- examples/mifare-desfire-format.c
+++ examples/mifare-desfire-format.c
@@ -12,44 +12,40 @@
 
 #include <freefare.h>
 
+#define MAX_NFC_DEVICES 32
+
 uint8_t key_data_picc[8] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 
 struct {
-    bool interactive;
-} format_options = {
-    .interactive = true
+    bool noconfirm;
+} options = {
+    .noconfirm = false
 };
 
 static void
 usage(char *progname)
 {
-    fprintf(stderr, "usage: %s [-y] [-K 11223344AABBCCDD]\n", progname);
+    fprintf(stderr, "usage: %s [-d device] [-y] [-K 11223344AABBCCDD]\n", progname);
     fprintf(stderr, "\nOptions:\n");
-    fprintf(stderr, "  -y     Do not ask for confirmation (dangerous)\n");
-    fprintf(stderr, "  -K     Provide another PICC key than the default one\n");
+    fprintf(stderr, "  -d  NFC device string\n");
+    fprintf(stderr, "  -y  Do not ask for confirmation (dangerous)\n");
+    fprintf(stderr, "  -K  Provide another PICC key than the default one\n");
 }
 
-int
-main(int argc, char *argv[])
+static int
+getopts(int argc, char **argv)
 {
-    int ch;
-    int error = EXIT_SUCCESS;
-    nfc_device *device = NULL;
-    FreefareTag *tags = NULL;
-
-    while ((ch = getopt(argc, argv, "hyK:")) != -1) {
-	switch (ch) {
+    int opt;
+    while ((opt = getopt(argc, argv, "hyK:")) != -1) {
+	switch (opt) {
 	case 'h':
-	    usage(argv[0]);
-	    exit(EXIT_SUCCESS);
-	    break;
+	    return 1; /* help requested */
 	case 'y':
-	    format_options.interactive = false;
+	    options.noconfirm = true;
 	    break;
 	case 'K':
 	    if (strlen(optarg) != 16) {
-		usage(argv[0]);
-		exit(EXIT_FAILURE);
+		return -1;
 	    }
 	    uint64_t n = strtoull(optarg, NULL, 16);
 	    int i;
@@ -59,70 +55,92 @@ main(int argc, char *argv[])
 	    }
 	    break;
 	default:
-	    usage(argv[0]);
-	    exit(EXIT_FAILURE);
+	    return -1;
 	}
     }
     // Remaining args, if any, are in argv[optind .. (argc-1)]
+    return 0;
+}
 
-    nfc_connstring devices[8];
-    size_t device_count;
-
+int
+main(int argc, char *argv[])
+{
+    int res;
     nfc_context *context;
+    nfc_connstring devices[MAX_NFC_DEVICES];
+    size_t device_count;
+    nfc_device *device;
+    FreefareTag *tags;
+
+    /* Parse options */
+    res = getopts(argc, argv);
+    if(res != 0) {
+	usage(argv[0]);
+	return (res < 0);
+    }
+
+    /* Initialize libnfc */
     nfc_init(&context);
     if (context == NULL)
 	errx(EXIT_FAILURE, "Unable to init libnfc (malloc)");
 
-    device_count = nfc_list_devices(context, devices, 8);
+    /* List NFC devices */
+    device_count = nfc_list_devices(context, devices, MAX_NFC_DEVICES);
     if (device_count <= 0)
 	errx(EXIT_FAILURE, "No NFC device found.");
 
-    for (size_t d = 0; (!error) && (d < device_count); d++) {
+    /* Scan devices */
+    for (size_t d = 0; d < device_count; d++) {
+
+	/* Open the device */
 	device = nfc_open(context, devices[d]);
 	if (!device) {
 	    warnx("nfc_open() failed.");
-	    error = EXIT_FAILURE;
 	    continue;
 	}
 
+	/* List tags */
 	tags = freefare_get_tags(device);
 	if (!tags) {
 	    nfc_close(device);
 	    errx(EXIT_FAILURE, "Error listing Mifare DESFire tags.");
 	}
 
-	for (int i = 0; (!error) && tags[i]; i++) {
-	    if (MIFARE_DESFIRE != freefare_get_tag_type(tags[i]))
+	/* Scan tags */
+	for (int i = 0; tags[i]; i++) {
+	    FreefareTag tag = tags[i];
+
+	    /* Ignore non-DESFire tags */
+	    if (MIFARE_DESFIRE != freefare_get_tag_type(tag)) {
 		continue;
+	    }
 
-	    char *tag_uid = freefare_get_tag_uid(tags[i]);
-	    char buffer[BUFSIZ];
+	    /* Get tag UID */
+	    char *tag_uid = freefare_get_tag_uid(tag);
+	    printf("Found %s with UID %s. ", freefare_get_tag_friendly_name(tag), tag_uid);
 
-	    printf("Found %s with UID %s. ", freefare_get_tag_friendly_name(tags[i]), tag_uid);
+	    /* Ask for confirmation */
 	    bool format = true;
-	    if (format_options.interactive) {
+	    char buffer[BUFSIZ];
+	    if (!options.unconfirmed) {
 		printf("Format [yN] ");
 		fgets(buffer, BUFSIZ, stdin);
 		format = ((buffer[0] == 'y') || (buffer[0] == 'Y'));
-	    } else {
-		printf("\n");
 	    }
 
+	    /* Perform operation if requested */
 	    if (format) {
-		int res;
-
-		res = mifare_desfire_connect(tags[i]);
+		/* Connect to the tag */
+		res = mifare_desfire_connect(tag);
 		if (res < 0) {
 		    warnx("Can't connect to Mifare DESFire target.");
-		    error = EXIT_FAILURE;
 		    break;
 		}
 
 		MifareDESFireKey key_picc = mifare_desfire_des_key_new_with_version(key_data_picc);
-		res = mifare_desfire_authenticate(tags[i], 0, key_picc);
+		res = mifare_desfire_authenticate(tag, 0, key_picc);
 		if (res < 0) {
 		    warnx("Can't authenticate on Mifare DESFire target.");
-		    error = EXIT_FAILURE;
 		    break;
 		}
 		mifare_desfire_key_free(key_picc);
@@ -133,13 +151,12 @@ main(int argc, char *argv[])
 		// bit2 equal to 1b, CreateApplication and DeleteApplication commands are allowed without PICC master key authentication
 		// bit1 equal to 1b, GetApplicationIDs, and GetKeySettings are allowed without PICC master key authentication
 		// bit0 equal to 1b, PICC masterkey MAY be frozen or changeable
-		res = mifare_desfire_change_key_settings(tags[i], 0x0F);
+		res = mifare_desfire_change_key_settings(tag, 0x0F);
 		if (res < 0)
 		    errx(EXIT_FAILURE, "ChangeKeySettings failed");
-		res = mifare_desfire_format_picc(tags[i]);
+		res = mifare_desfire_format_picc(tag);
 		if (res < 0) {
 		    warn("Can't format PICC.");
-		    error = EXIT_FAILURE;
 		    break;
 		}
 
@@ -153,6 +170,7 @@ main(int argc, char *argv[])
 	nfc_close(device);
     }
     nfc_exit(context);
-    exit(error);
+
+    return 0;
 } /* main() */
 
